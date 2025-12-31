05dfc38a642db62cdb92fb91006dd28d7d0abf01
Nitish Sharma <ernitishsharma361@gmail.com>
2025-12-31 17:27:41 +0530

    home page done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/Nav"
+import Home from "./components/Home"
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="./Avangerlogo.png" alt="" />
+      </div>
+      <ul>
+        <li>All Characters <i class="ri-menu-5-line"></i></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
